0851cf9eafe99ff79904d439a6367e32c852ee9a
chop028 <chopf028@gmail.com>
2023-11-24 00:34:18 +0900

    주문내역 조금

--- OrderDetailForm/Form1.Designer.cs
+++ OrderDetailForm/Form1.Designer.cs
@@ -37,42 +37,49 @@ namespace OrderDetailForm
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.DBGrid = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.showList = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(6, 4);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 24);
+            this.label1.Size = new System.Drawing.Size(57, 12);
             this.label1.TabIndex = 1;
             this.label1.Text = "주문 내역";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 110);
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 24);
+            this.label2.Size = new System.Drawing.Size(57, 12);
             this.label2.TabIndex = 2;
             this.label2.Text = "주문 일시";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(132, 110);
+            this.textBox1.Location = new System.Drawing.Point(71, 55);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(318, 35);
+            this.textBox1.Size = new System.Drawing.Size(173, 21);
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "2023년 11월 1일 오후 02:30";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(132, 189);
+            this.textBox2.Location = new System.Drawing.Point(71, 94);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(318, 35);
+            this.textBox2.Size = new System.Drawing.Size(173, 21);
             this.textBox2.TabIndex = 4;
             this.textBox2.Text = "0";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
@@ -80,26 +87,29 @@ namespace OrderDetailForm
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 192);
+            this.label3.Location = new System.Drawing.Point(6, 96);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 24);
+            this.label3.Size = new System.Drawing.Size(57, 12);
             this.label3.TabIndex = 5;
             this.label3.Text = "주문 번호";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 278);
+            this.label4.Location = new System.Drawing.Point(6, 139);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(106, 24);
+            this.label4.Size = new System.Drawing.Size(53, 12);
             this.label4.TabIndex = 6;
             this.label4.Text = "결제금액";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(132, 278);
+            this.textBox3.Location = new System.Drawing.Point(71, 139);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(318, 35);
+            this.textBox3.Size = new System.Drawing.Size(173, 21);
             this.textBox3.TabIndex = 7;
             this.textBox3.Text = "1800";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
@@ -107,29 +117,62 @@ namespace OrderDetailForm
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 400);
+            this.label6.Location = new System.Drawing.Point(6, 200);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(114, 24);
+            this.label6.Size = new System.Drawing.Size(57, 12);
             this.label6.TabIndex = 8;
             this.label6.Text = "주문 항목";
             // 
-            // dataGridView1
+            // DBGrid
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 467);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 82;
-            this.dataGridView1.RowTemplate.Height = 37;
-            this.dataGridView1.Size = new System.Drawing.Size(803, 253);
-            this.dataGridView1.TabIndex = 9;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.DBGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DBGrid.Location = new System.Drawing.Point(9, 234);
+            this.DBGrid.Margin = new System.Windows.Forms.Padding(2);
+            this.DBGrid.Name = "DBGrid";
+            this.DBGrid.RowHeadersWidth = 82;
+            this.DBGrid.RowTemplate.Height = 37;
+            this.DBGrid.Size = new System.Drawing.Size(432, 126);
+            this.DBGrid.TabIndex = 9;
+            this.DBGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 21);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "삭제";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(488, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 19);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "검색";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // showList
+            // 
+            this.showList.Location = new System.Drawing.Point(170, 365);
+            this.showList.Name = "showList";
+            this.showList.Size = new System.Drawing.Size(74, 21);
+            this.showList.TabIndex = 10;
+            this.showList.Text = "내역 보기";
+            this.showList.UseVisualStyleBackColor = true;
+            this.showList.Click += new System.EventHandler(this.showList_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1242, 850);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(895, 425);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.showList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DBGrid);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label4);
@@ -138,9 +181,11 @@ namespace OrderDetailForm
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,7 +201,10 @@ namespace OrderDetailForm
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DBGrid;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button showList;
     }
 }
 
--- OrderDetailForm/Form1.cs
+++ OrderDetailForm/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,38 @@ namespace OrderDetailForm
 {
     public partial class Form1 : Form
     {
+        private int SelectedRowIndex;
+        private void showList_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "User Id=admin; Password=admin; Data Source=(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = xe)) ); ";
+
+                // SQL 쿼리 수정
+                string commandString = "SELECT ORDER_ID, PAYMENT_DATE, TOTAL_AMOUNT FROM payment";
+
+                using (OracleDataAdapter DBAdapter = new OracleDataAdapter(commandString, connectionString))
+                {
+                    DataSet DS = new DataSet();
+                    DBAdapter.Fill(DS, "payment");
+
+                    // DBGrid 설정 (DataGridView)
+                    // 예: 자동 열 생성
+                    DBGrid.AutoGenerateColumns = true;
+
+                    DBGrid.DataSource = DS.Tables["payment"].DefaultView;
+                }
+            }
+            catch (DataException DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -33,6 +66,11 @@ namespace OrderDetailForm
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
